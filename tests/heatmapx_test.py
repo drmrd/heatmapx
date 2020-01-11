b47ce93cce4c766dad65b59a4c3a8586f2d8a754
drmrd <hello@danielmoore.xyz>
2020-01-11 18:01:56 -0500

    Create a skeletal temperature_graph function

--- tests/heatmapx_test.py
+++ tests/heatmapx_test.py
@@ -1,5 +1,15 @@
 import heatmapx as hx
+import networkx as nx
 
 
-def test_version():
-    assert hx.__version__ == '0.1.0'
+class TestHeatmapXPackaging:
+    def test_version(self):
+        assert hx.__version__ == '0.1.0'
+
+
+class TestTemperatureGraph:
+    def test_given_a_graph_and_source_nodes_returns_a_new_graph(self):
+        G = nx.Graph()
+        G_temperatures = hx.temperature_graph(G, source_nodes=[])
+        assert isinstance(G_temperatures, nx.Graph)
+        assert G_temperatures != G
