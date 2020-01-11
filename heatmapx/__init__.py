f59bc362ca985966c7ea97118853c78b4c645f51
drmrd <hello@danielmoore.xyz>
2020-01-11 18:01:56 -0500

    Create a skeletal temperature_graph function

--- heatmapx/__init__.py
+++ heatmapx/__init__.py
@@ -1,1 +1,13 @@
 __version__ = '0.1.0'
+
+from typing import Iterable
+
+import networkx as nx
+
+
+def temperature_graph(
+    G: nx.Graph,
+    source_nodes: Iterable,
+) -> nx.Graph:
+    T = nx.Graph()
+    return T
